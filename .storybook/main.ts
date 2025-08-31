074cddb7670c96c0d90cb7824ce09c21ed6d14c5
kweonsikyung <ekrk6632@naver.com>
2025-09-01 02:22:07 +0900

    refactor: clean up Storybook configuration and improve alias handling

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,39 +1,42 @@
-// .storybook/main.ts
-import type { StorybookConfig } from '@storybook/nextjs-vite';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import path from 'path';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../src/ui/**/*.stories.@(ts|tsx|mdx)',
-    '../src/ui/views/Sample/**/*.stories.@(ts|tsx|mdx)', 
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/ui/**/*.stories.@(ts|tsx|mdx)",
   ],
   addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
-    name: '@storybook/nextjs-vite',
+    name: "@storybook/nextjs-vite",
     options: {},
   },
-  viteFinal: async (config) => {
+  staticDirs: ["../public"],
+
+  viteFinal: async (config, { configType }) => {
     config.plugins = [...(config.plugins ?? []), vanillaExtractPlugin()];
-    config.resolve = {
-      ...(config.resolve ?? {}),
-      alias: {
-        ...(config.resolve?.alias ?? {}),
-        '@': path.resolve(__dirname, '../src'),
-      },
-    };
+
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+    if (configType === "PRODUCTION") {
+      config.base = "/DaenggleJeju-Client/";
+    }
+
     return config;
   },
-
-  staticDirs: ['../public'],
 };
 
 export default config;
