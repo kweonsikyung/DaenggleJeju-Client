7247b5c57298ed3764357ca789b5ebc179d1dacf
kweonsikyung <ekrk6632@naver.com>
2025-08-31 19:10:30 +0900

    feat: ChipMapList component in Map

---
+++ src/ui/atoms/Chip/ChipMapList/ChipMapList.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import * as s from "./ChipMapList.css";
+
+export interface ChipMapListProps {
+  /** 칩 중앙 텍스트 */
+  text: string;
+  /** 텍스트에 대해 표시할 숫자 */
+  cnt: number;
+  /** 칩 클릭 핸들러 */
+  onLocationListClick: () => void;
+}
+
+export function ChipMapList({
+  text,
+  cnt,
+  onLocationListClick,
+}: ChipMapListProps) {
+  return (
+    <button className={s.locationListButton} onClick={onLocationListClick}>
+      <span>
+        <Image
+          src="/assets/icon12/bullet-list_line-white.svg"
+          alt="목록"
+          width={12}
+          height={12}
+        />
+      </span>
+      <div>
+        <span className={s.locationListText}>{text}</span>
+        <span className={s.locationListCount}>{cnt}</span>
+      </div>
+    </button>
+  );
+}
