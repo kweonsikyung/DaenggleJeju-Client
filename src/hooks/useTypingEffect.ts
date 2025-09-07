c99f90c96a1e391f204370775b1ab85a53a20589
kweonsikyung <ekrk6632@naver.com>
2025-09-08 04:23:31 +0900

    feat: useTypingEffect hooks

---
+++ src/hooks/useTypingEffect.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+/**
+ * 텍스트를 타이핑하는 것처럼 보여주는 효과를 위한 커스텀 훅
+ * @param fullText - 최종적으로 표시할 전체 텍스트
+ * @param speed - 타이핑 속도 (ms)
+ */
+export function useTypingEffect(fullText: string, speed = 50) {
+  const [typedText, setTypedText] = useState("");
+
+  useEffect(() => {
+    setTypedText("");
+
+    if (fullText) {
+      let i = 0;
+      const intervalId = setInterval(() => {
+        setTypedText((prev) => prev + fullText.charAt(i));
+        i++;
+        if (i >= fullText.length) {
+          clearInterval(intervalId);
+        }
+      }, speed);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [fullText, speed]);
+
+  return typedText;
+}
