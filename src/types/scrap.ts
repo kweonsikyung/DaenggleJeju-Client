6d0749176e8097ba73df4b1b03d0d44b849d6dc2
kweonsikyung <ekrk6632@naver.com>
2025-09-13 00:17:07 +0900

    feat: implement API functions and types for`scrap`

---
+++ src/types/scrap.ts
@@ -0,0 +1,53 @@
+/** 스크랩 대상의 타입 */
+export type ScrapType = "place" | "daenggle";
+
+/**
+ * 사용자 본인이 스크랩한 항목 조회 Request
+ * @description GET /scraps/my 요청의 Query Parameter 타입
+ * @property type - 스크랩 대상 타입 (place | daenggle)
+ * @property limit - 한 번에 가져올 스크랩 개수
+ * @property offset - 가져올 스크랩의 시작 위치
+ */
+export interface GetScrapListReq {
+  type: ScrapType;
+  limit?: number;
+  offset?: number;
+}
+
+/**
+ * 사용자 본인이 스크랩한 항목 조회 Response
+ * @description GET /scraps/my 요청의 성공 응답 타입
+ * @property total - 전체 스크랩 개수
+ * @property items - 스크랩된 항목들의 배열
+
+ */
+export interface GetScrapListRes {
+  total: number;
+  items: [];
+}
+
+/**
+ * 스크랩을 추가/삭제 Request
+ * @description POST /scraps 요청의 Body 타입
+ * @property id - 스크랩할 대상의 ID
+ * @property type - 스크랩 대상 타입 (place | daenggle)
+ */
+export interface PostScrapReq {
+  id: number;
+  type: ScrapType;
+}
+
+/**
+ * 스크랩을 추가/삭제 Response
+ * @description POST /scraps 요청의 성공 응답 타입
+ * @property type - 스크랩 대상 타입 (place | daenggle)
+ * @property id - 스크랩한 대상의 ID
+ * @property scraped - 스크랩 상태 (true: 스크랩 추가됨, false: 스크랩 삭제됨)
+ * @property message - 서버에서 반환하는 메시지
+ */
+export interface PostScrapRes {
+  type: ScrapType;
+  id: number;
+  scraped: boolean;
+  message: string;
+}
