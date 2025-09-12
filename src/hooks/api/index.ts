94c84fe0c7f35a22a618fd567b1ba20014f9cfcb
kweonsikyung <ekrk6632@naver.com>
2025-09-13 00:47:32 +0900

    rename: change documentation folder for SWR data fetching hooks

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -1,16 +1,16 @@
-/** @file queries/index.ts
+/** @file hooks/api/index.ts
  * @module queries
  * @description SWR 키, 데이터 패칭 훅 모음 (문서 전용)
  *
  * @note 이 파일은 **배럴 파일로 사용하지 않습니다.**
  *       각 쿼리 훅은 필요한 곳에서만 개별 import 하세요.
  *       불필요한 데이터 패칭 훅이 번들에 포함되는 것을 방지합니다.
- * 
+ *
  * ## 디렉터리 가이드
  * - useContents.ts  : 콘텐츠 목록 패칭 훅
  *
  * ## import 가이드
- * // import { useMe } from "@/queries/useContents";
+ * import { useMe } from "@/hooks/api/useContents";
  */
 
 // 문서 전용 파일입니다. 코드/exports 를 추가하지 마세요.
