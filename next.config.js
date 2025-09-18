3140ff2baae29b226495466842e08af5d4343d75
kweonsikyung <ekrk6632@naver.com>
2025-09-18 20:23:53 +0900

    feat: add additional image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["tong.visitkorea.or.kr", "i.ytimg.com", "img1.kakaocdn.net"],
+    domains: [
+      "tong.visitkorea.or.kr",
+      "i.ytimg.com",
+      "img1.kakaocdn.net",
+      "kareumstay.com",
+      "api.cdn.visitjeju.net",
+      "cdn.visitkorea.or.kr",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
