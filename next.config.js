0433404922115a9eb8bf634d307b3927f4d44f59
kweonsikyung <ekrk6632@naver.com>
2025-10-27 02:34:56 +0900

    feat: update image configuration to use remote patterns instead of domains

--- next.config.js
+++ next.config.js
@@ -12,17 +12,35 @@ const nextConfig = {
     ];
   },
   images: {
-    domains: [
-      "tong.visitkorea.or.kr",
-      "i.ytimg.com",
-      "img1.kakaocdn.net",
-      "kareumstay.com",
-      "api.cdn.visitjeju.net",
-      "cdn.visitkorea.or.kr",
-      "lh3.googleusercontent.com",
-      "storage.doopedia.co.kr",
-      "googleusercontent.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // 모든 https 호스트 허용
+      },
+      {
+        protocol: "http",
+        hostname: "**", // 모든 http 호스트 허용 (보안상 덜 권장됨)
+      },
     ],
+    // domains: [
+    //   "tong.visitkorea.or.kr",
+    //   "i.ytimg.com",
+    //   "img1.kakaocdn.net",
+    //   "kareumstay.com",
+    //   "api.cdn.visitjeju.net",
+    //   "cdn.visitkorea.or.kr",
+    //   "lh3.googleusercontent.com",
+    //   "storage.doopedia.co.kr",
+    //   "googleusercontent.com",
+    //   "yaimg.yanolja.com",
+    //   "peton.me",
+    //   "mblogthumb-phinf.pstatic.net",
+    //   "a0.muscache.com",
+    //   "t1.daumcdn.net",
+    //   "t1.kakaocdn.net",
+    //   "postfiles.pstatic.net",
+    //   "d12zq4w4guyljn.cloudfront.net",
+    // ],
   },
 };
 
