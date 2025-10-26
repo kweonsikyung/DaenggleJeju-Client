56165e05d667d82e4b9fafc45ae303de63c9f1e4
kweonsikyung <ekrk6632@naver.com>
2025-10-27 05:02:49 +0900

    fix: update dog info display in PlaceDetailClient component

--- src/app/detail/[contentId]/page.tsx
+++ src/app/detail/[contentId]/page.tsx
@@ -457,7 +457,7 @@ function PlaceDetailClient({ contentId }: { contentId: number }) {
                     profileImageUrl={getRandomAvatar()}
                     userName={review.writer.pet?.name || "댕글제주"}
                     dogInfo={
-                      `${review.writer.pet?.sizeLabelKo} · ${review.writer.pet?.sizeLabelKo}` ||
+                      `${review.writer.pet?.breedNameKo} · ${review.writer.pet?.sizeLabelKo}` ||
                       "중형견(6~15kg)"
                     }
                     rating={review.rating}
