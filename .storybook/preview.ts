f5cfaaa6a41a876aeddc2ff77bd99555c8a5e354
kweonsikyung <kweonsikyung@gmail.com>
2026-05-06 18:17:41 +0900

    feat: add autodocs and chromatic github actions workflow

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,14 +8,14 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-
     a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
       test: "todo",
     },
+    docs: {
+      autodocs: true,
+    },
   },
+  tags: ['autodocs'],
 };
 
 export default preview;
