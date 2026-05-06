3c1042ea70178488c9a7b642eab693a61dbe2559
kweonsikyung <sikyung.kweon@naverlabs.com>
2026-05-06 14:24:18 +0900

    fix: add turbopack config to resolve webpack conflict in Next.js 16

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   async rewrites() {
     return [
       {
@@ -15,32 +16,13 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**", // 모든 https 호스트 허용
+        hostname: "**",
       },
       {
         protocol: "http",
-        hostname: "**", // 모든 http 호스트 허용 (보안상 덜 권장됨)
+        hostname: "**",
       },
     ],
-    // domains: [
-    //   "tong.visitkorea.or.kr",
-    //   "i.ytimg.com",
-    //   "img1.kakaocdn.net",
-    //   "kareumstay.com",
-    //   "api.cdn.visitjeju.net",
-    //   "cdn.visitkorea.or.kr",
-    //   "lh3.googleusercontent.com",
-    //   "storage.doopedia.co.kr",
-    //   "googleusercontent.com",
-    //   "yaimg.yanolja.com",
-    //   "peton.me",
-    //   "mblogthumb-phinf.pstatic.net",
-    //   "a0.muscache.com",
-    //   "t1.daumcdn.net",
-    //   "t1.kakaocdn.net",
-    //   "postfiles.pstatic.net",
-    //   "d12zq4w4guyljn.cloudfront.net",
-    // ],
   },
 };
 
